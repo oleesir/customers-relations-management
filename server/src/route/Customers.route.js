75dfba50741a285fe7a3dfec5101df1528300338
oleesir <oleesir@gmail.com>
2019-12-02 13:32:32 +0100

    ft(get-single-customer): Implement ft-get-single-customer
    -  Implement ft-get-single-customer
    
    [Finishes]

--- server/src/route/Customers.route.js
+++ server/src/route/Customers.route.js
@@ -8,12 +8,16 @@ import asyncErrorHandler from '../middleware/asyncErrorHandler';
 const router = Router();
 
 const { verifyToken, authorizeRoles } = Authorization;
-const { createCustomer } = CustomerController;
-const { validateCreateCustomer } = Validation;
+const { createCustomer, getSingleCustomer } = CustomerController;
+const { validateCreateCustomer, validateGetCustomer } = Validation;
 
 router.post('/', verifyToken,
   authorizeRoles(['staff']),
   validateCreateCustomer,
   asyncErrorHandler(createCustomer));
+
+router.get('/:id', verifyToken,
+  validateGetCustomer,
+  asyncErrorHandler(getSingleCustomer));
 
 export default router;
--- server/test/customer.test.js
+++ server/test/customer.test.js
@@ -8,7 +8,8 @@ import Fixtures from './helpers/fixtures';
 const {
   staffToken, newCustomer, emptyCustomer, emptyCustomerFirstName,
   emptyCustomerLastName, emptyCustomerEmail, emptyPhoneNumber,
-  emptyaddress
+  emptyaddress, rightCustomerId, wrongCustomerId, adminToken,
+  expiredToken
 } = Fixtures;
 const URL = '/api/v1';
 
@@ -74,6 +75,35 @@ describe('Customer Routes', () => {
         });
     });
 
+    it('should send an error message if token is not provided', (done) => {
+      request(app)
+        .post(`${URL}/customers`)
+        .send(newCustomer)
+        .set('Authorization', `Bearer ${''}`)
+        .expect(401)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body).to.have.property('message').equal('Please provide a token');
+          if (err) return done(err);
+          done();
+        });
+    });
+
+
+    it('should send an error message if token is not expired', (done) => {
+      request(app)
+        .post(`${URL}/customers`)
+        .send(newCustomer)
+        .set('Authorization', `Bearer ${expiredToken}`)
+        .expect(401)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body).to.have.property('error').equal('User authorization token is expired');
+          if (err) return done(err);
+          done();
+        });
+    });
+
     it('should not create customer with empty email field', (done) => {
       request(app)
         .post(`${URL}/customers`)
@@ -111,6 +141,48 @@ describe('Customer Routes', () => {
         .end((err, res) => {
           expect(res.body).to.have.property('status').eql(400);
           expect(res.body.error[0]).to.equal('"address" is not allowed to be empty');
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+  describe('Get Customer', () => {
+    it('should allow a staff get a single customer', (done) => {
+      request(app)
+        .get(`${URL}/customers/${rightCustomerId}`)
+        .set('Authorization', `Bearer ${staffToken}`)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.have.property('status').eql(200);
+          expect(res.body.data).to.have.property('customerId').eql(rightCustomerId);
+          if (err) return done(err);
+          done();
+        });
+    });
+
+
+    it('should not get a customerId that does not belong to a customer', (done) => {
+      request(app)
+        .get(`${URL}/customers/${wrongCustomerId}`)
+        .set('Authorization', `Bearer ${staffToken}`)
+        .expect(404)
+        .end((err, res) => {
+          expect(res.body).to.have.property('status').eql(404);
+          expect(res.body).to.have.property('message').to.eql('Customer does not exists');
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    it('should get any account for admin', (done) => {
+      request(app)
+        .get(`${URL}/customers/${rightCustomerId}`)
+        .set('Authorization', `Bearer ${adminToken}`)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.have.property('status').eql(200);
+          expect(res.body.data).to.have.property('customerId').eql(rightCustomerId);
           if (err) return done(err);
           done();
         });
